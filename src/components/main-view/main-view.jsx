dbfe652549bb53d596bbd87bde1f9c78523aa086
Alen245 <akiyanovski24@gmail.com>
2023-04-17 09:40:49 -0400

    comments

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -4,14 +4,17 @@ import { MovieView } from "../movie-view/movie-view";
 import { LoginView } from "../login-view/login-view";
 
 export const MainView = () => {
-  const [movies, setMovies] = useState([]);
-  const [selectedMovie, setSelectedMovie] = useState(null);
-  const [user, setUser] = useState(null);
-  const [token, setToken] = useState(null);
+  // state variables using useState hook
+  const [movies, setMovies] = useState([]); // array of movies
+  const [selectedMovie, setSelectedMovie] = useState(null); // selected movie object
+  const [user, setUser] = useState(null); // user object
+  const [token, setToken] = useState(null); // JWT token
 
+  // if user is not logged in, show the LoginView component
   if (!user) {
     return (
       <LoginView
+        // callback function for when user logs in
         onLoggedIn={(user, token) => {
           setUser(user);
           setToken(token);
@@ -20,10 +23,12 @@ export const MainView = () => {
     );
   }
 
+  // useEffect hook to fetch movie data from API when component mounts
   useEffect(() => {
     fetch("https://moviepi24.herokuapp.com/movies")
       .then((response) => response.json())
       .then((data) => {
+        // map data received from API to an array of objects with desired properties
         const moviesFromApi = data.map((movie) => {
           return {
             id: movie._id,
@@ -35,38 +40,46 @@ export const MainView = () => {
             actors: movie.Actors,
           };
         });
-
+        // set the movies state to the new array of movie objects
         setMovies(moviesFromApi);
       });
   }, []);
 
+  // useEffect hook to fetch movie data with authentication when token changes
   useEffect(() => {
+    // if token is not set, return from hook
     if (!token) {
       return;
     }
-
+    // fetch movie data with Authorization header using the token
     fetch("https://moviepi24.herokuapp.com/movies", {
       headers: { Authorization: `Bearer ${token}` }
     })
       .then((response) => response.json())
       .then((data) => {
+        // log the data received from API to console
         console.log(data);
       });
   }, [token]);
 
+  // if user is not logged in, show the LoginView component
   if (!user) {
     return <LoginView onLoggedIn={(user) => setUser(user)} />;
   }
 
+  // if selectedMovie is set, show the MovieView component for the selected movie
   if (selectedMovie) {
     return (
       <MovieView movie={selectedMovie} onBackClick={() => setSelectedMovie(null)} />
     );
   }
 
+  // if the movies array is empty, show a message
   if (movies.length === 0) {
     return <div>The list is empty!</div>;
   }
+
+  // render the movie cards for each movie object in the movies array
   return (
     <div>
       {movies.map((movie) => (
@@ -78,6 +91,7 @@ export const MainView = () => {
           }}
         />
       ))}
+      {/* button to logout and reset user and token states */}
       <button onClick={() => { setUser(null); setToken(null); }}>Logout</button>
     </div>
   );
