101044c92577c48f8d239501943bca3595d11e98
Alen245 <akiyanovski24@gmail.com>
2023-04-17 09:52:17 -0400

    comments login-view

--- src/components/login-view.jsx
+++ src/components/login-view.jsx
@@ -1,36 +1,41 @@
 import { useState } from "react";
 
+// a component that takes a callback function called onLoggedIn as a prop
 export const LoginView = ({ onLoggedIn }) => {
+
+  // set initial state of username and password using useState hooks
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
 
+  // define a function to handle the form submission
   const handleSubmit = (event) => {
-    // this prevents the default behavior of the form which is to reload the entire page
-    event.preventDefault();
+    event.preventDefault(); // prevent form from reloading the page
 
+    // construct the login data object
     const data = {
       access: username,
       secret: password
     };
 
+    // send a POST request to the login API with the data object
     fetch("https://openlibrary.org/account/login.json", {
       method: "POST",
       body: JSON.stringify(data)
     })
-      .then((response) => response.json())
+      .then((response) => response.json()) // parse the response as JSON
       .then((data) => {
-        console.log("Login response: ", data);
-        if (data.user) {
-          onLoggedIn(data.user, data.token);
+        if (data.user) { // if the login was successful
+          onLoggedIn(data.user, data.token); // call the onLoggedIn function with user and token data
         } else {
-          alert("No such user");
+          alert("No such user"); // show an alert if login failed
         }
       })
       .catch((e) => {
-        alert("Something went wrong");
+        alert("Something went wrong"); // show an alert if an error occurred
       });
   };
 
+  // render a form with input fields for username and password, and a submit button
   return (
     <form onSubmit={handleSubmit}>
       <label>
