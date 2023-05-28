c1a38a31eb2f96226b1ccc28f9bb06cd90d3f4b2
Alen245 <akiyanovski24@gmail.com>
2023-05-28 17:37:17 -0400

    try again

--- src/components/profile-view/favorite-movies.jsx
+++ src/components/profile-view/favorite-movies.jsx
@@ -1,22 +1,27 @@
-import React from 'react'
+import React from 'react';
 import { Link } from 'react-router-dom';
 
-export default function FavoriteMovies({ favoriteMovieList }) {
+export default function FavoriteMovies({ favoriteMovieList, onRemoveFavorite }) {
+    const removeFav = (id) => {
+        if (typeof onRemoveFavorite === "function") {
+            onRemoveFavorite(id);
+        }
+    };
+
     return (
         <div>
             <h2>Favorite Movies</h2>
-            {favoriteMovieList.map((movies) => {
-                return (
-                    <div key={movies._id}>
-                        <img src={movies.ImagePath} />
-                        <Link to={`/movies/${movies._id}`}>
-                            <h4>{movies.Title}</h4>
-                        </Link>
-                        <button variant="secondary" onClick={() => removeFav(movies._id)}>Remove From List</button>
-                    </div>
-                )
-            })
-            }
+            {favoriteMovieList.map((movie) => (
+                <div key={movie._id}>
+                    <img src={movie.ImagePath} alt={movie.Title} />
+                    <Link to={`/movies/${movie._id}`}>
+                        <h4>{movie.Title}</h4>
+                    </Link>
+                    <button variant="secondary" onClick={() => removeFav(movie._id)}>
+                        Remove From List
+                    </button>
+                </div>
+            ))}
         </div>
-    )
+    );
 }
